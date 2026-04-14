c3a9f84decf4b809e80d7bc4ee5ac69b44769f49
kawani0824 <kawanishi_yuuki@koushi-intec.co.jp>
2026-04-14 13:02:24 +0900

    kadai_15

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,19 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	//フィールド
+	private int gear=1;
+	private int speed=10;
+	public void changeGear( final int afterGear ) {
+		System.out.println("ギア"+gear+"から"+afterGear+"に切り替えました");
+		gear = afterGear;
+		if((gear>=1)&&(gear<=5)) {
+			speed = gear*10;
+		}else {
+			speed = 10;
+		}
+	}
+	public void run() {
+		System.out.println("速度は時速"+speed+"kmです");
+	}
+}
